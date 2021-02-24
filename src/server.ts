83378ade37a403d67b291b96bbe8627e628a463e
denisgodoy <denisgodoy1@gmail.com>
2021-02-24 17:41:45 -0300

    separate server listen

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,3 @@
-import 'reflect-metadata';
-import express from 'express';
-import './database';
-import { router } from './routes';
+import { app } from './app';
 
-const app = express();
-
-app.use(express.json());
-app.use(router);
-
-app.listen(3333, () => console.log("Server running!"));
+app.listen(3333, () => console.log("Server listening on port:3333"));
